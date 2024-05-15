86d6be9480bc57a2e3f2834f414599d1b941371b
Bumbi <annika.borgesson@gmail.com>
2024-05-15 19:47:19 +0200

    Create view for one single dog #18

--- dogs/urls.py
+++ dogs/urls.py
@@ -3,4 +3,5 @@ from dogs import views
 
 urlpatterns = [
   path("dogs/", views.DogsListView.as_view()),
+  path("dogs/<int:pk>", views.DogsListView.as_view()),
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Dog
@@ -12,4 +13,14 @@ class DogsListView(APIView):
   def get(self, request):
     dogs = Dog.objects.all()
     serializer = DogSerializer(dogs, many=True)
+    return Response(serializer.data)
+  
+    
+class DogDetailView(APIView):
+  """
+  Retrieve the details of a single dog available for adoption.
+  """
+  def get(self, request, pk):
+    dog = get_object_or_404(Dog, pk=pk)
+    serializer = DogSerializer(dog)
     return Response(serializer.data)
