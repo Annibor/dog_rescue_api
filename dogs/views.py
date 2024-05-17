fe5f06f38f8827cfabd65c89186ccbd2f20161e2
Bumbi <annika.borgesson@gmail.com>
2024-05-17 09:43:18 +0200

    Add filter and search function #11 # 12
    Chagned the DogListView to add in filter and search fucntion for the dog list view

--- dogs/views.py
+++ dogs/views.py
@@ -1,19 +1,27 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import filters
 from .models import Dog
-from .serializers import DogSerializer
+from .serializers import DogsSerializer
+
+
 
 # Create your views here.
-class DogsListView(APIView):
+class DogsListView(generics.ListAPIView):
   """
   List of all dogs available for adoption.
+  Users can filter and search for dogs by their name, 
+  breed and other attributes.
   """
 
-  def get(self, request):
-    dogs = Dog.objects.all()
-    serializer = DogSerializer(dogs, many=True)
-    return Response(serializer.data)
+  queryset = Dog.objects.all()
+  serializer_class = DogsSerializer
+  filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+  filterset_fields = ['name', 'breed', 'age', 'gender', 'good_with_children']
+  search_fields = ['name', 'breed', 'age', 'gender', 'good_with_children']
   
     
 class DogDetailView(APIView):
@@ -22,7 +30,5 @@ class DogDetailView(APIView):
   """
   def get(self, request, pk):
     dog = get_object_or_404(Dog, pk=pk)
-    serializer = DogSerializer(dog)
+    serializer = DogsSerializer(dog)
     return Response(serializer.data)
-
-class Post
